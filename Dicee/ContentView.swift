cf5a8ae434b85be70df5cb1997206a1d29f1ebf4
kokikudo <massanmesu@icloud.com>
2022-01-31 11:58:04 +0900

    inicial commit: @Stateを使った値の更新処理を実装

--- Dicee/ContentView.swift
+++ Dicee/ContentView.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State var leftDiceeNumber = 1
+    @State var rightDiceeNumber = 1
+
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: leftDiceeNumber)
+                    DiceView(n: rightDiceeNumber)
+                }.padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.leftDiceeNumber = Int.random(in: 1...6)
+                    self.rightDiceeNumber = Int.random(in: 1...6)
+
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+
+    let n: Int
+
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1.0, contentMode: .fit)
             .padding()
     }
 }
@@ -19,3 +59,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
